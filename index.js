4f259cae0710c8e6ed549747581474d63f981e9d
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-16 18:29:14 +0400

    add token

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { validationResult } from "express-validator"
 
 import UserModel from './models/User.js'
 
-mongoose.connect('mongodb+srv://admin:12345@cluster0.dzsrkfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://admin:12345@cluster0.dzsrkfs.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
    .then(() => console.log('db ok'))
    .catch((err) => console.log('db error', err))
 
@@ -18,25 +18,37 @@ const app = express()
 app.use(express.json())
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-   const errors = validationResult(req)
-   if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array())
+   try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+         return res.status(400).json(errors.array())
+      }
+
+      const password = req.body.password
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(password, salt)
+
+      const doc = new UserModel({
+         email: req.body.email,
+         fullName: req.body.fullName,
+         avatarUrl: req.body.avatarUrl,
+         passwordHash: hash
+      })
+
+      const user = await doc.save()
+
+      const token = jwt.sign({ _id: req._id }, 'secret123', { expiresIn: '30d' })
+
+      const { passwordHash, ...userData } = user._doc
+
+      res.json({ ...userData, token })
+
+   } catch (err) {
+      console.log(err)
+      res.status(500).json({
+         message: 'Не удалось зарегестрироваться'
+      })
    }
-
-   const password = req.body.password
-   const salt = await bcrypt.genSalt(10)
-   const passwordHash = await bcrypt.hash(password, salt)
-
-   const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash
-   })
-
-   const user = await doc.save()
-
-   res.json(user)
 })
 
 app.listen(4444, (err) => {
