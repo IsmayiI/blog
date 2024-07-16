72ab7deffe8df22d88394c2a671c908fa6dfdfde
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-16 17:09:40 +0400

    add user model

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 
-
 import express from "express"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
+
+mongoose.connect('mongodb+srv://admin:12345@cluster0.dzsrkfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+   .then(() => console.log('db ok'))
+   .catch((err) => console.log('db error', err))
+
 
 const app = express()
+
+app.use(express.json())
+
+app.post('/auth/login', (req, res) => {
+   const token = jwt.sign({
+      email: req.body.email,
+      fullname: "Ismayil Ismayilov"
+   }, 'secret123')
+
+   res.json({
+      success: true,
+      token
+   })
+})
 
 app.get('/', (req, res) => {
    res.send('OK!!')
