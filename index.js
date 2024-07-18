21d8ef06cfd409eb150730f770edca016d900077
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-18 15:38:34 +0400

    add Post model

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 import express from "express"
 import mongoose from "mongoose"
 
-import { registerValidation } from './validations/auth.js'
+import { registerValidation, loginValidation } from './validations.js'
 import checkAuth from "./utils/CheckAuth.js"
 import * as UserController from "./controllers/UserController.js"
+import * as PostController from "./controllers/PostController.js"
 
 mongoose.connect('mongodb+srv://admin:12345@cluster0.dzsrkfs.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
    .then(() => console.log('db ok'))
@@ -15,11 +16,11 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/auth/login', UserController.login)
-
+app.post('/auth/login', loginValidation, UserController.login)
 app.post('/auth/register', registerValidation, UserController.register)
-
 app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('')
 
 app.listen(4444, (err) => {
    if (err) {
