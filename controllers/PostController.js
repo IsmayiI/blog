48ce913e68c811d965e3699c2286985c2fa316c5
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-18 16:02:07 +0400

    add create post function

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,14 +1,22 @@
 
+import { validationResult } from 'express-validator'
 import PostModel from '../models/Post.js'
 
 export const create = async (req, res) => {
    try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+         return res.status(400).json(errors.array())
+      }
+
+
+
       const doc = new PostModel({
          title: req.body.title,
          text: req.body.text,
          tags: req.body.tags,
          imageUrl: req.body.imageUrl,
-         user: req.userID
+         user: req.userId
       })
 
       const post = await doc.save()
