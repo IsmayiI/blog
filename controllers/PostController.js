bfb2856a04426093e3606a425b283182cba9f978
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-18 17:12:28 +0400

    add get functions

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,6 +2,38 @@
 import { validationResult } from 'express-validator'
 import PostModel from '../models/Post.js'
 
+export const getAll = async (req, res) => {
+   try {
+      const posts = await PostModel.find().populate('user').exec()
+
+      res.json(posts)
+   } catch (err) {
+      res.status(500).json({
+         message: 'Не удалось получить статьи'
+      })
+   }
+}
+
+export const getOne = async (req, res) => {
+   try {
+      const postId = req.params.id
+
+
+      const post = await PostModel.findByIdAndUpdate(
+         { _id: postId },
+         { $inc: { viewsCount: 1 } },
+         { returnDocument: 'after' }
+      ).populate('ser')
+
+      res.json(post)
+
+   } catch (err) {
+      res.status(500).json({
+         message: 'Не удалось получить статью'
+      })
+   }
+}
+
 export const create = async (req, res) => {
    try {
       const errors = validationResult(req)
