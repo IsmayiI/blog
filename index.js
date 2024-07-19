6f62186463401ae058112011e83265f33ced2c8d
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-19 15:54:53 +0400

    add multer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 import express from "express"
 import mongoose from "mongoose"
+import multer from "multer"
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
 import checkAuth from "./utils/CheckAuth.js"
@@ -14,11 +15,29 @@ mongoose.connect('mongodb+srv://admin:12345@cluster0.dzsrkfs.mongodb.net/blog?re
 
 const app = express()
 
+const storage = multer.diskStorage({
+   destination: (_, __, cb) => {
+      cb(null, 'uploads')
+   },
+   filename: (_, file, cb) => {
+      cb(null, file.originalname)
+   },
+})
+
+const upload = multer({ storage })
+
 app.use(express.json())
+app.use('/uploads', express.static('uploads'))
 
 app.post('/auth/login', loginValidation, UserController.login)
 app.post('/auth/register', registerValidation, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+   res.json({
+      url: `/uploads/${req.file.originalname}`
+   })
+})
 
 app.get('/posts', PostController.getAll)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
