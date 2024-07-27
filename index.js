55d6c565096ef8af26ae31799e9b76efad25c98b
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-07-27 18:48:52 +0400

    change uploads dir

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express"
 import mongoose from "mongoose"
 import multer from "multer"
 import cors from "cors"
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
 
 import { registerValidation, loginValidation, postCreateValidation, postUpdateValidation } from './validations.js'
 import { PostController, UserController } from "./controllers/index.js"
@@ -13,11 +16,17 @@ mongoose.connect('mongodb+srv://admin:12345@cluster0.dzsrkfs.mongodb.net/blog?re
    .catch((err) => console.log('db error', err))
 
 
+const userHomeDir = os.homedir();
+const uploadsDir = path.join(userHomeDir, 'uploads');
+
 const app = express()
 
 const storage = multer.diskStorage({
    destination: (_, __, cb) => {
-      cb(null, 'uploads')
+      if (!fs.existsSync(uploadsDir)) {
+         fs.mkdirSync(uploadsDir, { recursive: true });
+      }
+      cb(null, uploadsDir);
    },
    filename: (_, file, cb) => {
       cb(null, file.originalname)
@@ -28,7 +37,7 @@ const upload = multer({ storage })
 
 app.use(express.json())
 app.use(cors())
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', express.static(uploadsDir))
 
 app.get('/auth/me', checkAuth, UserController.getMe)
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
